ba6c24164e5dc6efde5d29ab4297039854e8883b
Lucas Martins <lucastm@outlook.com.br>
2022-07-14 20:38:37 -0400

    JS orçamento e seguros finalizados

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//Ativar links MENU
 const links = document.querySelectorAll(".header-menu a");
 
 
@@ -10,3 +11,16 @@ function ativarLinks (link) {
 }
 
 links.forEach(ativarLinks);
+
+// Ativar itens do orçamento
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto (parametro) {
+  const elemento = document.getElementById(parametro);
+  if(elemento) {
+    elemento.checked = true;
+  }
+}
+
+parametros.forEach(ativarProduto);
